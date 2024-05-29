2da73d67e538ecc55f74e116e3dda425b441a5fb
ferreus <pxrxzit@gmail.com>
2024-05-29 11:04:42 +0330

    fix: change animation trigger

--- components/chip/Chip.tsx
+++ components/chip/Chip.tsx
@@ -19,7 +19,7 @@ const Chip = () => {
       ease: "power2.inOut",
       scrollTrigger: {
         trigger: "#chip",
-        start: "100% top",
+        start: "100% bottom",
         markers: true
       },
     });
